01612228ab09ff9436b30ba5515118e8c2e2f4a4
Juandavidc08 <>
2023-11-29 12:28:53 +0000

    Set up structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,31 @@
-console.log('Connected!')
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectanswer(){
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubstractQuestion() {
+
+}
+
+function displayMultiplyQustion() {
+
+}
